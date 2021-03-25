b0dff69f440cfe02496249d5a304923184d30d3b
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-03-25 19:43:16 -0300

    Add update and delete methods to Product API

--- sales-node-api/src/controllers/product.controller.js
+++ sales-node-api/src/controllers/product.controller.js
@@ -1,3 +1,5 @@
+const { response } = require("express")
+
 const my_database = new Map()
 
 const listAll = (req, res) => {
@@ -12,11 +14,32 @@ const create = (req, res) => {
 }
 
 const update = (req, res) => {
-  console.log('[CONTROLLER] ESTOU NO PUT!!!')
+  const id = parseInt(req.params.id, 10)
+
+  if (my_database.has(id)) {
+    const product = req.body
+    product.id = id
+    my_database.set(id, product)
+    res.status(200).send(product)
+  } else {
+    res.status(404).send({
+      message: `product with id (${id}) not found`
+    })
+  }
 }
 
 const remove = (req, res) => {
-  console.log('[CONTROLLER] ESTOU NO DELETE!!!')
+  const id = parseInt(req.params.id, 10)
+
+  if (my_database.has(id)) {
+    const product = my_database.get(id)
+    my_database.delete(product.id)
+    res.status(200).send(product)
+  } else {
+    res.status(404).send({
+      message: `product with id (${id}) not found`
+    })
+  }
 }
 
 module.exports = {
