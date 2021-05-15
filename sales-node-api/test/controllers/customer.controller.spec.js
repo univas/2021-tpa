2cac768a43766d54f9760250c04df2b90456a3fe
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-05-15 10:07:32 -0300

    Add nyc dependency to check the code coverage

--- sales-node-api/test/controllers/customer.controller.spec.js
+++ sales-node-api/test/controllers/customer.controller.spec.js
@@ -4,12 +4,12 @@ const expect = chai.expect
 const controller = require('../../src/controllers/customer.controller')
 
 describe('Customer Controller', () => {
-   it('Call create method should add new customer', () => {
-    const myCustomer = {
-      id: 123,
-      name: 'João'
-    }
+  const myCustomer = {
+    id: 123,
+    name: 'João'
+  }
 
+  it('Call create method should add new customer', () => {
     const req = {
       body: myCustomer
     }
@@ -36,7 +36,81 @@ describe('Customer Controller', () => {
     
     expect(res.send.calledOnce).to.be.true
     expect(parameter.length).to.be.equal(1)
-    expect(parameter[0].id).to.be.equal(123)
-    expect(parameter[0].name).to.be.equal('João')
+    expect(parameter[0].id).to.be.equal(myCustomer.id)
+    expect(parameter[0].name).to.be.equal(myCustomer.name)
+  })
+
+  it('Call update method with correct parameter should update customer', () => {
+    const req = {
+      body: myCustomer,
+      params: {
+        id: myCustomer.id
+      }
+    }
+
+    const res = {}
+    res.status = () => res
+    res.send = sinon.spy()
+
+    controller.update(req, res)
+
+    expect(res.send.calledOnce).to.be.true
+    expect(res.send.firstCall.args[0]).to.equal(myCustomer)
+  })
+
+  it('Call update method with invalid parameter should not update customer', () => {
+    const req = {
+      body: myCustomer,
+      params: {
+        id: 999
+      }
+    }
+
+    const res = {}
+    res.status = () => res
+    res.send = sinon.spy()
+
+    controller.update(req, res)
+
+    expect(res.send.calledOnce).to.be.true
+    expect(res.send.firstCall.args[0]).to.be.eql({
+      message: `customer with id (${req.params.id}) not found`
+    })
+  })
+
+  it('Call remove method with correct parameter should remove the customer', () => {
+    const req = {
+      params: {
+        id: myCustomer.id
+      }
+    }
+
+    const res = {}
+    res.status = () => res
+    res.send = sinon.spy()
+
+    controller.remove(req, res)
+
+    expect(res.send.calledOnce).to.be.true
+    expect(res.send.firstCall.args[0]).to.be.equal(myCustomer)
+  })
+
+  it('Call remove method with invalid parameter should not delete the customer', () => {
+    const req = {
+      params: {
+        id: 54
+      }
+    }
+
+    const res = {}
+    res.status = () => res
+    res.send = sinon.spy()
+
+    controller.remove(req, res)
+
+    expect(res.send.calledOnce).to.be.true
+    expect(res.send.firstCall.args[0]).to.be.eql({
+      message: `customer with id (${req.params.id}) not found`
+    })
   })
 })
